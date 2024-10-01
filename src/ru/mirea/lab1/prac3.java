3df0f7c2488c6cd6ed42b155cb76be2df0674474
modesik <korchsergey86360dm@gmail.com>
2024-10-01 17:19:39 +0300

    Update prac3.java

--- src/ru/mirea/lab1/prac3.java
+++ src/ru/mirea/lab1/prac3.java
@@ -4,6 +4,6 @@ import java.util.Scanner;
 
 public class prac3 {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world, sergey");
     }
 }
